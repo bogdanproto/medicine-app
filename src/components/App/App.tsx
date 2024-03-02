093b26f53d80f8cd9633b17ce079a7fcc6dfdb64
Bogdan Protasenko <b.protasenko@gmail.com>
2024-03-02 01:27:47 +0100

    add refresh user

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,21 @@ import { AuthPage } from 'components/Auth';
 import { SharedLayout } from 'components/SharedLayout/SharedLayout';
 import { routePage } from 'const';
 import { NotFound, PrivateRoute, RestrictedRoute } from 'pages';
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import { Route, Routes } from 'react-router';
+import { refreshUser } from 'services/redux/authSlice/operations';
+import { useTypeDispatch } from 'services/redux/customHook/typeHooks';
 
 const Shop = lazy(() => import('../../pages/Shop/Shop'));
 const Cart = lazy(() => import('../../pages/Cart/Cart'));
 
 export const App = () => {
+  const dispatch = useTypeDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route
